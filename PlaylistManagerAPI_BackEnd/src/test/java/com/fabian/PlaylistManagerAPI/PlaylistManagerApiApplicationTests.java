4512cd57787ba4697585de8425972d2ff258bc71
Fabian obdulio Baca Quitian <fabian.baca.14@gmail.com>
2024-12-14 11:01:46 -0500

    first commit

---
+++ PlaylistManagerAPI_BackEnd/src/test/java/com/fabian/PlaylistManagerAPI/PlaylistManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabian.PlaylistManagerAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaylistManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
